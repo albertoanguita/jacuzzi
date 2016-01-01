e7b207533edb89520a9834f452e914cb10bec870
Alberto Anguita <alberto.anguita@gmail.com>
2016-01-01 21:24:36 +0100

    Added ErrorFactory for asynchronous handling of error

---
+++ src/main/java/jacz/util/log/ErrorFactory.java
@@ -0,0 +1,36 @@
+package jacz.util.log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * This class allows generating detailed error messages and redirecting them to an error handled implementation
+ */
+public class ErrorFactory {
+
+    public static void reportError(ErrorHandler errorHandler, String message, Object... data) {
+        PrintStream ps = new PrintStream(new ByteArrayOutputStream());
+        ps.println("ERROR THROWN");
+        ps.println("------------");
+        ps.println(new SimpleDateFormat("YYY/MM/dd-HH:mm:ss:SSS").format(new Date()));
+        ps.println();
+        printError(ps, message, data);
+        ps.println("-------------------------------------------------------");
+        ps.println("Stack trace:");
+        new RuntimeException().printStackTrace(ps);
+        errorHandler.errorRaised(ps.toString());
+        ps.close();
+    }
+
+    private static void printError(PrintStream ps, String message, Object... data) {
+        ps.println("Message: " + message);
+        ps.println("Data:");
+        int i = 0;
+        for (Object o : data) {
+            ps.println("Data " + i + ": " + o.toString());
+        }
+    }
+
+}
---
+++ src/main/java/jacz/util/log/ErrorHandler.java
@@ -0,0 +1,9 @@
+package jacz.util.log;
+
+/**
+ * Created by Alberto on 01/01/2016.
+ */
+public interface ErrorHandler {
+
+    void errorRaised(String errorMessage);
+}

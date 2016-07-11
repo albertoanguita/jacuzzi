0ce24bcd59459bccc36c43c514c7fc39a1c6a89c
alberto <alberto.anguita@gmail.com>
2016-07-11 10:51:13 +0200

    Fix synch issues, simplify

--- src/main/java/org/aanguita/jacuzzi/fsm/TimedFSM.java
+++ src/main/java/org/aanguita/jacuzzi/fsm/TimedFSM.java
@@ -1,9 +1,7 @@
 package org.aanguita.jacuzzi.fsm;
 
-import org.aanguita.jacuzzi.concurrency.timer.TimerAction;
 import org.aanguita.jacuzzi.concurrency.timer.Timer;
-
-import java.util.concurrent.atomic.AtomicBoolean;
+import org.aanguita.jacuzzi.concurrency.timer.TimerAction;
 
 /**
  * FSM with timeout capability
@@ -14,8 +12,6 @@ public class TimedFSM<T, Y> extends GenericFSM<T, Y> implements TimerAction {
 
     private final Timer timer;
 
-    private AtomicBoolean timedOut;
-
     public TimedFSM(TimedFSMAction<T, Y> timedFSMAction, long timeoutMillis) {
         this("unnamedTimedFSM", timedFSMAction, timeoutMillis);
     }
@@ -23,12 +19,7 @@ public class TimedFSM<T, Y> extends GenericFSM<T, Y> implements TimerAction {
     public TimedFSM(String name, TimedFSMAction<T, Y> timedFSMAction, long timeoutMillis) {
         super(name, timedFSMAction);
         this.timedFSMAction = timedFSMAction;
-        timedOut = new AtomicBoolean(false);
         timer = new Timer(timeoutMillis, this, false, name);
-    }
-
-    private void stopTimer() {
-        timer.stop();
     }
 
     private void restartTimer() {
@@ -36,50 +27,42 @@ public class TimedFSM<T, Y> extends GenericFSM<T, Y> implements TimerAction {
     }
 
     @Override
-    public synchronized boolean newInput(Y input) {
-        if (!timedOut.get()) {
-            // we stop the timer during the new input invocation so we don't get timeouts if the method takes long to complete
-            stopTimer();
-            boolean active = super.newInput(input);
-            if (!active) {
-                kill();
-            } else {
-                restartTimer();
-            }
-            return active;
+    public boolean start() {
+        if (super.start()) {
+            // if it is active after start, initiate the timer. Otherwise, the timer is not initiated and thus it does not need to be killed later
+            restartTimer();
+            return true;
         } else {
+            timer.kill();
             return false;
         }
     }
 
     @Override
-    public synchronized boolean start() {
-        boolean result = super.start();
-        if (result) {
-            // if it is active after start, initiate the timer. Otherwise, the timer is not initiated and thus it does not need to be killed later
+    public boolean newInput(Y input) {
+        timer.stop();
+        if (super.newInput(input)) {
             restartTimer();
-        }
-        return result;
-    }
-
-    public void stop() {
-        super.stop();
-        kill();
-    }
-
-    public void kill() {
-        timer.kill();
-        synchronized (this) {
-            timedOut.set(true);
+            return true;
+        } else {
+            timer.kill();
+            return false;
         }
     }
 
     @Override
-    public synchronized Long wakeUp(Timer timer) {
+    public void stop() {
+        super.stop();
+        timer.kill();
+    }
+
+    @Override
+    public Long wakeUp(Timer timer) {
         // the timer has jumped -> no activity reached this FSM during the specified time, the FSM finishes
         // we first check that the FSM is still active
-        if (active.get()) {
-            timedOut.set(true);
+        if (active.getAndSet(false)) {
+            active.set(false);
+            //timedOut.set(true);
             timedFSMAction.timedOut(currentState);
         }
         // the timer dies

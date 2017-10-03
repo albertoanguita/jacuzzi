29af7003188f50f5cf7cfd6bc5c9482ff317e7f1
Alberto Anguita <aanguita@net4things.com>
2017-10-03 08:08:24 +0200

    Use atomic reference in blocking state

--- jacuzzicore/src/main/java/org/aanguita/jacuzzi/event/hub/EventHubSubscriber.java
+++ jacuzzicore/src/main/java/org/aanguita/jacuzzi/event/hub/EventHubSubscriber.java
@@ -3,6 +3,9 @@ package org.aanguita.jacuzzi.event.hub;
 /**
  * @author aanguita
  *         23/09/2016
+ *
+ *         todo make special notifications when it is connected to an event hub and when it is disconnected, with default
+ *         empty implementations
  */
 public interface EventHubSubscriber {
 

936b0446f6a05cf8d0ee7b0cd11ed9e0d2b63ef1
Alberto Anguita <aanguita@net4things.com>
2017-11-06 14:03:36 +0100

    Minor fix

--- jacuzzicore/src/main/java/org/aanguita/jacuzzi/numeric/NumericUtil.java
+++ jacuzzicore/src/main/java/org/aanguita/jacuzzi/numeric/NumericUtil.java
@@ -460,7 +460,7 @@ public class NumericUtil {
         }
     }
 
-    private static double round(double value, int precision) {
+    public static double round(double value, int precision) {
         int scale = (int) Math.pow(10, precision);
         return (double) Math.round(value * scale) / scale;
     }

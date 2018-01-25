a9ae8adf5c6615eaaa5c91424a4003510c45bfa8
Alberto Anguita <aanguita@net4things.com>
2018-01-25 16:15:11 +0100

    Improve tests

--- jacuzzicore/src/test/java/org/aanguita/jacuzzi/concurrency/PeriodicTaskReminderTest.java
+++ jacuzzicore/src/test/java/org/aanguita/jacuzzi/concurrency/PeriodicTaskReminderTest.java
@@ -12,14 +12,22 @@ public class PeriodicTaskReminderTest {
 
     @Test
     public void test() {
-        PeriodicTaskReminder.getInstance("name").addPeriodicTask("task", new Runnable() {
+        PeriodicTaskReminder.getInstance("name").addPeriodicTask("task1", new Runnable() {
             @Override
             public void run() {
-                System.out.println("Running...");
+                System.out.println("Running 1...");
                 ThreadUtil.safeSleep(5000);
-                System.out.println("Finished running");
+                System.out.println("Finished running 1");
             }
         }, false, 1000, false);
+        PeriodicTaskReminder.getInstance("name").addPeriodicTask("task2", new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running 2...");
+                ThreadUtil.safeSleep(3000);
+                System.out.println("Finished running 2");
+            }
+        }, false, 2000, false);
 
 
         ThreadUtil.safeSleep(20000);
